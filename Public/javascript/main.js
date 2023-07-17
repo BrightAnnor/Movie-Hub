4704992d135951b41e045c2baa71d342302e2c86
BrightAnnor <annor170bright@gmail.com>
2023-07-17 21:02:41 +0000

    Movie Hub Project

---
+++ Public/javascript/main.js
@@ -0,0 +1,32 @@
+// current movies
+const moviesTitle = ['Fast X','John Wick','Merlin','Extraction']
+const moviesContainer = document.getElementById('moviesContainer')
+const apiKey = '4f9990b9'
+
+// generate movie
+const movieRequest = async(title)=>{
+    return await
+    axios.get(`http://www.omdbapi.com/?apikey=${apiKey}&t=${title}`)
+}
+movieRequest()
+
+
+
+// create a funtion to display the movies in the array
+
+const generateMovies = async()=>{
+    moviesTitle.forEach(async (title)=>{
+        const {data} = await movieRequest(title)
+        // console.log(movie)
+        moviesContainer.innerHTML +=`<div class="col">
+        <img src="${data.Poster}" alt="" style="max-width:70% ; border-radius: 7px; box-shadow:inset(0,0.9,0.5) ;">
+       <span class="title-design">${data.Title}</span>
+       </div> 
+       `
+    })
+
+}
+generateMovies()
+
+
+// target the container for the movies.
